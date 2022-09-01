7b17fe8062e549d213452b4fe0cae405aaa56d92
Illia Kononenko <ilyanes88@gmail.com>
2022-09-01 10:27:49 +0300

    v1.1

--- src/services/fetchAnswers.js
+++ src/services/fetchAnswers.js
@@ -4,7 +4,6 @@ axios.defaults.baseURL = "https://api.chucknorris.io/jokes";
 
 const fetchAnswers = async () => {
   const response = await axios.get("/random");
-  console.log(response.data.value);
   return response.data.value;
 };
 
